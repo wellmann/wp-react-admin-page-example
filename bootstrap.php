d246016d1f7e328c2e9fcff50bc8dcc60fc76e74
Kevin Wellmann <kevin@wellmann.io>
2024-12-11 23:41:40 +0100

    code style: adhere to WordPress Coding Standards

--- bootstrap.php
+++ bootstrap.php
@@ -1,7 +1,5 @@
 <?php
-
 /**
- * @wordpress-plugin
  * Plugin Name: React Admin Page Example
  * Plugin URI: https://github.com/wellmann/wp-react-admin-page-example
  * Description: WordPress admin page developed with React and Gutenberg components.
@@ -12,20 +10,24 @@
  * Text Domain: wp-react-admin-page-example
  * Domain Path: /lang
  * GitHub Plugin URI: https://github.com/wellmann/wp-react-admin-page-example
+ *
+ * @package kwio/wp-react-admin-page-example
  */
 
-namespace KWIO\ReactAdminPage;
+namespace KWIO\React_Admin_Page;
 
-if (!defined('ABSPATH')) {
-    exit;
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
 }
 
-define(__NAMESPACE__ . '\\MAIN_FILE', __FILE__);
-define(__NAMESPACE__ . '\\BASENAME', plugin_basename(MAIN_FILE));
-define(__NAMESPACE__ . '\\DIR_PATH', plugin_dir_path(MAIN_FILE));
-define(__NAMESPACE__ . '\\DIR_URL', plugin_dir_url(MAIN_FILE));
-define(__NAMESPACE__ . '\\ADMIN_PAGE_SLUG', 'kwio-react-page');
+define( __NAMESPACE__ . '\\MAIN_FILE', __FILE__ );
+define( __NAMESPACE__ . '\\BASENAME', plugin_basename( MAIN_FILE ) );
+define( __NAMESPACE__ . '\\DIR_PATH', plugin_dir_path( MAIN_FILE ) );
+define( __NAMESPACE__ . '\\DIR_URL', plugin_dir_url( MAIN_FILE ) );
+define( __NAMESPACE__ . '\\ADMIN_PAGE_SLUG', 'kwio-react-page' );
 
-if (file_exists(DIR_PATH . 'vendor/autoload.php')) {
-    require_once DIR_PATH . 'vendor/autoload.php';
+if ( file_exists( DIR_PATH . 'vendor/autoload.php' ) ) {
+	require_once DIR_PATH . 'vendor/autoload.php';
 }
+
+require_once DIR_PATH . 'src/hooks.php';
--- src/hooks.php
+++ src/hooks.php
@@ -1,63 +1,80 @@
 <?php
+/**
+ * Registers a custom administration page in WordPress using React for rendering.
+ *
+ * @package kwio/wp-react-admin-page-example
+ */
 
-namespace KWIO\ReactAdminPage;
+namespace KWIO\React_Admin_Page;
 
-add_action('admin_menu', function () {
-    add_menu_page(
-        __('React Page', 'wp-react-admin-page-example'),
-        __('React Page', 'wp-react-admin-page-example'),
-        'manage_options',
-        ADMIN_PAGE_SLUG,
-        function () {
-            printf(
-                '<div class="wrap" id="root" data-title="%s"></div>',
-                get_admin_page_title()
-            );
-        },
-        'data:image/svg+xml;base64,' . base64_encode(file_get_contents(DIR_PATH . '/assets/images/icon.svg'))
-    );
-});
+add_action(
+	'admin_menu',
+	function () {
+		add_menu_page(
+			__( 'React Page', 'wp-react-admin-page-example' ),
+			__( 'React Page', 'wp-react-admin-page-example' ),
+			'manage_options',
+			ADMIN_PAGE_SLUG,
+			function () {
+				printf(
+					'<div class="wrap" id="root" data-title="%s"></div>',
+					esc_attr( get_admin_page_title() )
+				);
+			},
+            // phpcs:disable WordPress.PHP.DiscouragedPHPFunctions.obfuscation_base64_encode
+            // phpcs:disable WordPress.WP.AlternativeFunctions.file_get_contents_file_get_contents
+			'data:image/svg+xml;base64,' . base64_encode( file_get_contents( DIR_PATH . '/assets/images/icon.svg' ) )
+		);
+	}
+);
 
-add_action('admin_enqueue_scripts', function ($hookSuffix) {
-    if ($hookSuffix !== 'toplevel_page_' . ADMIN_PAGE_SLUG) {
-        return;
-    }
+add_action(
+	'admin_enqueue_scripts',
+	function ( $hook_suffix ) {
+		if ( 'toplevel_page_' . ADMIN_PAGE_SLUG !== $hook_suffix ) {
+			return;
+		}
 
-    $assetManifestFile = DIR_PATH . 'dist/app.asset.php';
-    if (!file_exists($assetManifestFile)) {
-        return;
-    }
+		$asset_manifest_file = DIR_PATH . 'dist/app.asset.php';
+		if ( ! file_exists( $asset_manifest_file ) ) {
+			return;
+		}
 
-    $assetManifest = include $assetManifestFile;
+		$asset_manifest = include $asset_manifest_file;
 
-    wp_enqueue_script(
-        ADMIN_PAGE_SLUG,
-        DIR_URL . 'dist/app.js',
-        $assetManifest['dependencies'],
-        $assetManifest['version']
-    );
+		wp_enqueue_script(
+			ADMIN_PAGE_SLUG,
+			DIR_URL . 'dist/app.js',
+			$asset_manifest['dependencies'],
+			$asset_manifest['version'],
+			true
+		);
 
-    wp_set_script_translations(
-        ADMIN_PAGE_SLUG,
-        'wp-react-admin-page-example',
-        DIR_PATH . 'lang'
-    );
+		wp_set_script_translations(
+			ADMIN_PAGE_SLUG,
+			'wp-react-admin-page-example',
+			DIR_PATH . 'lang'
+		);
 
-    wp_enqueue_style(
-        ADMIN_PAGE_SLUG,
-        DIR_URL . 'dist/app.css',
-        ['wp-components'],
-        filemtime(DIR_PATH . 'dist/app.css')
-    );
+		wp_enqueue_style(
+			ADMIN_PAGE_SLUG,
+			DIR_URL . 'dist/app.css',
+			array( 'wp-components' ),
+			filemtime( DIR_PATH . 'dist/app.css' )
+		);
 
-    remove_action('admin_print_styles', 'print_emoji_styles');
-    remove_action('admin_print_scripts', 'print_emoji_detection_script');
-});
+		remove_action( 'admin_print_styles', 'print_emoji_styles' );
+		remove_action( 'admin_print_scripts', 'print_emoji_detection_script' );
+	}
+);
 
-add_action('init', function () {
-    load_plugin_textdomain(
-        'wp-react-admin-page-example',
-        false,
-        dirname(BASENAME)  . '/lang'
-    );
-});
+add_action(
+	'init',
+	function () {
+		load_plugin_textdomain(
+			'wp-react-admin-page-example',
+			false,
+			dirname( BASENAME ) . '/lang'
+		);
+	}
+);
